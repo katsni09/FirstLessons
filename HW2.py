633347f37750b983cb8312c664435161598cfae2
katesnitko <katesnit19@gmail.com>
2023-09-04 17:03:13 -0400

    second exercise added

--- HW2.py
+++ HW2.py
@@ -28,3 +28,18 @@ else:
     elif choose_the_number == "AVER":
         print(int((first+second+third)/3))
 
+#second exercise
+# 2. Користувач вводить з клавіатури кількість метрів.
+# Залежно від вибору користувача програма переводить метри милі, дюйми або ярди. mi =m * 0.00062137
+
+inquiry = int(input("Enter your inquiry: "))
+revert = input("Enter MILES, INCHES, YARDS: ")
+if revert != "MILES" and revert != "INCHES" and revert != "YARDS":
+    print("Incorrect inquiry")
+else:
+    if revert == "MILES":
+        print(inquiry * 0.00062137)
+    elif revert == "INCHES":
+        print(inquiry * 39.370)
+    elif revert == "YARDS":
+        print(inquiry * 1.0936)
